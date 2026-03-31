f4729fde086d7c25c6b2446cadafd9acf68cbb81
zhal <github@zilnet.eu.org>
2026-03-31 15:12:30 +0800

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,8 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['Nangahure'] = array ('1'=>'Nangahure!remote7.vpnmurahjogja.my.id:31140','Nangahure@|@admin','Nangahure#|#oZKfqpqsmZ2To5OTqg==','Nangahure%Anugrah Network','Nangahure^anugrahnetwork.com','Nangahure&Rp','Nangahure*10','Nangahure(1','Nangahure)','Nangahure=10','Nangahure@!@disable');
+$data['Nangadero'] = array ('1'=>'Nangadero!remote7.vpnmurahjogja.my.id:31138','Nangadero@|@admin','Nangadero#|#oZKfqpqsmZ2To5OTqg==','Nangadero%Adzila Network','Nangadero^adzilanetwork.net','Nangadero&Rp','Nangadero*10','Nangadero(1','Nangadero)','Nangadero=10','Nangadero@!@disable');
+$data['FARON-BENGKEL'] = array ('1'=>'FARON-BENGKEL!192.168.40.1','FARON-BENGKEL@|@admin','FARON-BENGKEL#|#oZKfqpqsmZ2To5OTqg==','FARON-BENGKEL%Sistem Voucher','FARON-BENGKEL^sistemvoucher.net','FARON-BENGKEL&Rp','FARON-BENGKEL*10','FARON-BENGKEL(1','FARON-BENGKEL)','FARON-BENGKEL=10','FARON-BENGKEL@!@disable');
+$data['Demo'] = array ('1'=>'Demo!172.20.1.1','Demo@|@admin','Demo#|#mZWfoZ8=','Demo%Nazira Hotspot','Demo^Nazirahotspot.com','Demo&Rp','Demo*10','Demo(1','Demo)','Demo=10','Demo@!@disable');
